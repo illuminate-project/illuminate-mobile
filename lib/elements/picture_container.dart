a6dd5bf4acca49d68ec64e3c0763181e9f81856d
Robby Uriarte <robbyuriarte2@gmail.com>
2023-01-01 20:22:11 -0500

    Added Local Image Retrieval

--- lib/elements/picture_container.dart
+++ lib/elements/picture_container.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PictureContainer extends StatefulWidget with PreferredSizeWidget {
   const PictureContainer({super.key});
@@ -11,38 +14,59 @@ class PictureContainer extends StatefulWidget with PreferredSizeWidget {
 }
 
 class _PictureContainerState extends State<PictureContainer> {
-  void doNothing() {}
+  XFile? _selectedImage;
+
+  void _getImage() async {
+    XFile? image = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _selectedImage = image;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: double.infinity,
-        height: double.infinity,
-        margin: const EdgeInsets.only(
-            left: 30.0, right: 30.0, top: 30.0, bottom: 125.0),
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(217, 217, 217, 217),
-          border: Border.all(
-            width: 2.5,
+    return GestureDetector(
+      child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          margin: const EdgeInsets.only(
+              left: 30.0, right: 30.0, top: 30.0, bottom: 125.0),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(217, 217, 217, 217),
+            border: Border.all(
+              width: 2.5,
+            ),
+            borderRadius: BorderRadius.circular(10),
           ),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.add_photo_alternate_outlined),
-              iconSize: 100,
-              onPressed: () => doNothing(),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              'Add Image',
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            )
-          ],
-        ));
+          child: _selectedImage != null
+              ? Image.file(
+                  File(_selectedImage!.path),
+                  width: double.infinity,
+                  height: double.infinity,
+                )
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.add_photo_alternate_outlined,
+                      size: 100,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Add Image',
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )),
+      onTap: () {
+        if (_selectedImage == null) {
+          _getImage();
+        }
+      },
+    );
   }
 }
